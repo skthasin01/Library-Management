5995179464b8a0c167808dd0366f19880df2b676
Sheikh Thasin Rahman Shiplu <88823794+skthasin01@users.noreply.github.com>
2026-09-12 19:52:18 +0600

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.rwsendftbzgmnghpnmxy:Li12BraryMa@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 Sessionlocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
