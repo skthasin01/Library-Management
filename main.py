4e404f16fe51f5f458116cdb9e58f9ac4b0b5e2b
Sheikh Thasin Rahman Shiplu <88823794+skthasin01@users.noreply.github.com>
2026-09-20 14:07:21 +0600

    adding netlify link

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://library-management-bythasin.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
